33c13c360046e00f6fc9d9e33169f92ff91c9b11
KSN <scofield.michael339@gmail.com>
2025-08-18 13:39:42 +0700

    Fix: Fixing nodejs code for CI

--- app/src/marked.js
+++ app/src/marked.js
@@ -1,12 +1,18 @@
 const hljs = require('highlight.js');
-const marked = require('marked');
 
-marked.setOptions({
-  highlight: function(code) {
-    return hljs.highlightAuto(code).value;
+async function getMarked() {
+  const markedModule = await import('marked');
+  // For ESM, marked is usually the default export
+  const marked = markedModule.default;
+  if (typeof marked.setOptions !== 'function') {
+    throw new Error('marked.setOptions is not a function. Check marked import.');
   }
-});
-
-module.exports = async function getMarked() {
+  marked.setOptions({
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
   return marked;
-};
+}
+
+module.exports = getMarked;
--- app/src/posts.js
+++ app/src/posts.js
@@ -4,19 +4,18 @@ const fm = require('front-matter');
 const getMarked = require('./marked');
 const config = require('../config');
 
-async function createPost(filepath) {
+async function createPost(filename) {
   try {
-    const content = fs.readFileSync(path.join(config.dev.contentDir, filepath), 'utf-8');
+    const content = fs.readFileSync(path.join(config.dev.contentDir, filename), 'utf-8');
     const { attributes, body } = fm(content);
     const marked = await getMarked();
 
     return {
       ...attributes,                  // front-matter metadata (title, date, etc.)
-      body: marked(body),             // HTML content
-      slug: filepath.replace('.md', '') // file name without .md
+      body: marked(body)             // HTML content
     };
   } catch (err) {
-    console.error('Error processing file:', filepath, err);
+    console.error('Error processing file:', filename, err);
     throw err;
   }
 }
