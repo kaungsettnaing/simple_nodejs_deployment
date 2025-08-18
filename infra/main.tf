b20e53f64a13f239312526b67b9676ec06a0a963
KSN <scofield.michael339@gmail.com>
2025-08-18 22:20:10 +0700

    Feat: Added S3 bucket policy for cloudfront service access

--- infra/main.tf
+++ infra/main.tf
@@ -46,3 +46,26 @@ resource "aws_s3_bucket_public_access_block" "static_website_bucket" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.static_website_bucket.arn,
+      "${aws_s3_bucket.static_website_bucket.arn}/*",
+    ]
+  }
+}
+
