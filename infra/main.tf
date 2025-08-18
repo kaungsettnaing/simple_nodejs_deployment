f4c9f2e92469882310da8d9377b6fcad5ba8fc80
KSN <scofield.michael339@gmail.com>
2025-08-19 00:16:43 +0700

    Feat: Added cloudfront distribution in infra

--- infra/main.tf
+++ infra/main.tf
@@ -40,32 +40,81 @@ resource "aws_s3_bucket_ownership_controls" "static_website_bucket" {
 resource "aws_s3_bucket_public_access_block" "static_website_bucket" {
   bucket = aws_s3_bucket.static_website_bucket.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "oac-ksn-static" {
+  name                              = "oac-ksn-static"
+  description                       = "static website s3 Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+locals {
+  s3_origin_id = "s3-static-website-origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_website_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac-ksn-static.id
+    origin_id                = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  enabled             = true
+  default_root_object = "index.html"
+}
+
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+  statement {
+    sid     = "AllowCloudFrontServicePrincipal"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.static_website_bucket.id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::478340992676:distribution/E26TUU2N2I0E8T"]
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   bucket = aws_s3_bucket.static_website_bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
 }
-
-data "aws_iam_policy_document" "allow_access_from_cloudfront" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    actions = [
-      "s3:GetObject",
-    ]
-
-    resources = [
-      aws_s3_bucket.static_website_bucket.arn,
-      "${aws_s3_bucket.static_website_bucket.arn}/*",
-    ]
-  }
-}
-
