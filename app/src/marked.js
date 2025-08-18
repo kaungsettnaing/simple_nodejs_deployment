05f35a85cbc22f847b65eb74d32f5757e07312a3
KSN <scofield.michael339@gmail.com>
2025-08-18 12:48:33 +0700

    Fix: Fixing marked.js file for CI

--- app/src/marked.js
+++ app/src/marked.js
@@ -1,7 +1,11 @@
 const hljs = require('highlight.js');
 
 async function getMarked() {
-  const { marked } = await markedPromise;
+  const markedModule = await import('marked');
+  const marked = markedModule.default;
+  if (typeof marked.setOptions !== 'function') {
+    throw new Error('marked.setOptions is not a function. Check marked import.');
+  }
   marked.setOptions({
     highlight: function(code) {
       return hljs.highlightAuto(code).value;
