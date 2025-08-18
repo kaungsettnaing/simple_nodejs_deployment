6ff4cbbef26a2086b152b79910665b64459f676e
KSN <scofield.michael339@gmail.com>
2025-08-18 13:41:28 +0700

    Fix: Fixing nodejs code for CI

--- app/src/marked.js
+++ app/src/marked.js
@@ -2,9 +2,10 @@ const hljs = require('highlight.js');
 
 async function getMarked() {
   const markedModule = await import('marked');
-  // For ESM, marked is usually the default export
-  const marked = markedModule.default;
-  if (typeof marked.setOptions !== 'function') {
+  // Try all possible exports
+  const marked = markedModule.default || markedModule.marked || markedModule;
+  if (!marked || typeof marked.setOptions !== 'function') {
+    console.error('DEBUG markedModule:', markedModule); // Add this for debugging
     throw new Error('marked.setOptions is not a function. Check marked import.');
   }
   marked.setOptions({
