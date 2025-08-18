bfea79e9174b83d567f32835adaca0fe937eb926
KSN <scofield.michael339@gmail.com>
2025-08-18 13:44:02 +0700

    Fix: Fixing code for CI

--- app/src/posts.js
+++ app/src/posts.js
@@ -4,15 +4,22 @@ const fm = require('front-matter');
 const getMarked = require('./marked');
 const config = require('../config');
 
+function getSlug(filename, attributes) {
+  return attributes.slug || filename.replace(/\.md$/, '');
+}
+
 async function createPost(filename) {
   try {
     const content = fs.readFileSync(path.join(config.dev.contentDir, filename), 'utf-8');
     const { attributes, body } = fm(content);
     const marked = await getMarked();
+    const slug = getSlug(filename, attributes);
 
     return {
-      ...attributes,                  // front-matter metadata (title, date, etc.)
-      body: marked(body)             // HTML content
+      title: attributes.title || slug,
+      date: attributes.date,
+      slug,
+      body: marked(body)
     };
   } catch (err) {
     console.error('Error processing file:', filename, err);
