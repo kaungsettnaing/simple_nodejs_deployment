532725d59e49543fb341f5c0ecce0357cb66eec4
ksn <scofield.michael339@gmail.com>
2025-08-25 21:55:32 +0700

    Fix: Delete commented lines of code in aws backend tf file

--- aws-backend-infra/main.tf
+++ aws-backend-infra/main.tf
@@ -1,25 +1,4 @@
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket        = var.backend_name 
-#   force_destroy = true
-# }
-
-# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-#   bucket        = aws_s3_bucket.terraform_state.bucket 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
-
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "5.5.0"
