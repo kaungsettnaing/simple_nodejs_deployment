b42d6e76f09f60463f3ceac09ab26dccb2a70f5a
ksn <scofield.michael339@gmail.com>
2025-08-25 17:57:13 +0700

    Feat: Separate provider info from main.tf and used aws s3 module instead of individual resources

--- aws-backend-infra/main.tf
+++ aws-backend-infra/main.tf
@@ -1,36 +1,52 @@
-terraform {
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = var.backend_name 
+#   force_destroy = true
+# }
+
+# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+#   bucket        = aws_s3_bucket.terraform_state.bucket 
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.5.0"
+
+  bucket = var.backend_name
+  acl    = "private"
+
+  control_object_ownership = false
+  object_ownership         = "BucketOwnerPreferred"
+
+  tags = {
+    Name        = "NodeJS Static Website Bucket"
+    Environment = "Dev"
   }
-}
 
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = var.backend_name 
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
+  versioning = {
+    status = true
   }
-}
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
+  server_side_encryption_configuration = {
+      rule = {
+        apply_server_side_encryption_by_default = {
+          sse_algorithm = "AES256"
+        }
+      }
   }
+
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
